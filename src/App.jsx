f5b98d7ededd445a88ac58e37a6656f81f90eb3d
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-09-21 23:42:26 +0530

    reorganized code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/LandingPage/Home';
-import Contact from './pages/Layout/Contact/Contact';
-import Login from './pages/Layout/Login/Login';
-import Nopage from './pages/Layout/NoPage/NoPage';
-import Register from './pages/Layout/Register/Register';
-import Unauthorized from './pages/Unauthorized/Unauthorized';
+import { Router } from './routes/router';
 import { AuthProvider } from './utils/auth';
-import RequireAuth from './utils/requireAuth';
+
 
 function App() {
   return (
     <div>
       <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-              <Route index element={<Home/>} />
-              <Route exact path="register" element={<Register/>} />
-              <Route exact path='login' element={<Login/>} />
-              <Route exact path='contact' element={<RequireAuth><Contact/></RequireAuth>} />
-              <Route exact path='unauthorized' element={<Unauthorized/>} />
-              <Route exact path='*' element={<Nopage/>} />
-          </Routes>
-        </BrowserRouter>
+        <Router/>
       </AuthProvider>
     </div>
   );
