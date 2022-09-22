085f7f2fe870a033abf18b3a81c21e202be29b0c
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-09-23 00:58:27 +0530

    added UI part for getting NIC to register

--- src/pages/Layout/Register/Register.jsx
+++ src/pages/Layout/Register/Register.jsx
@@ -1,9 +1,126 @@
-import * as React from 'react';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Slide from "@mui/material/Slide";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 function Register() {
-    return ( <div>
-        <h1>Register Here..</h1>
-    </div> );
+  const [checked, setChecked] = React.useState(true);
+
+  useEffect(() => {
+    setChecked((prev) => !prev);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <>
+      <button onClick={(e) => setChecked((prev) => !prev)}>Slide</button>
+      <Slide
+        direction={checked ? "up" : "down"}
+        in={checked}
+        mountOnEnter
+        unmountOnExit
+      >
+        <div>
+          <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Register
+                </Typography>
+                <Box
+                  component="form"
+                  noValidate
+                  onSubmit={handleSubmit}
+                  sx={{ mt: 3 }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="nic"
+                        label="NIC"
+                        name="nic"
+                        autoComplete="nic"
+                      />
+                    </Grid>
+                    <Grid item xs={12}></Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    CONTINUE
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="#" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+              <Copyright sx={{ mt: 5 }} />
+            </Container>
+          </ThemeProvider>
+        </div>
+      </Slide>
+    </>
+  );
 }
 
 export default Register;
