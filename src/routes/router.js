dca946bb5482926e35ca8f2e2fad167ec84ce01d
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-09-26 11:24:52 +0530

    completed customer login by password method

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,25 +1,23 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 // import Login from "../views/login/Login";
 import CustomerHome from "../views/customer/CustomerHome";
 import Register from "../pages/Layout/Register/Register";
 import Login from "../views/login/Login";
-import Unauthorized from "../pages/Unauthorized/Unauthorized";
 import NoPage from "../pages/Layout/NoPage/NoPage";
 import Contact from '../pages/Layout/Contact/Contact';
 import RequireAuth from '../utils/requireAuth';
-
-export function Router(){
-  return(
+import { CUSTOMER } from "../utils/RolesList";
+export function Router() {
+  return (
     <BrowserRouter>
-          <Routes>
-              <Route path="/customerHome" element={<CustomerHome/>}/>
-              <Route exact path="/register" element={<Register/>} />
-              <Route exact path='/login' element={<Login/>} />
-              <Route exact path='/contact' element={<RequireAuth><Contact/></RequireAuth>} />
-              <Route exact path='/unauthorized' element={<Unauthorized/>} />
-              <Route exact path='*' element={<NoPage/>} />
-          </Routes>
-        </BrowserRouter>
+      <Routes>
+        <Route path="/customerHome" element={<RequireAuth allowedRoles={[CUSTOMER]}><CustomerHome /></RequireAuth>} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='*' element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
